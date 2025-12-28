be90933fd3095bc4a09cc97334d0be370052e07c
Asha8141 <ashavithanage9@gmail.com>
2025-12-29 00:16:08 +0530

    Add student management functionality

---
+++ sms.py
@@ -0,0 +1,109 @@
+from tkinter import *
+import time
+from tkinter import ttk
+
+import ttkthemes
+from tkinter import ttk
+
+#fuctionality part
+count=0
+text=''
+def slider():
+    global text,count
+    if count==len(s):
+        count=0
+        text=''
+    text=text+s[count]
+    sliderLabel.config(text=text)
+    count+=1
+    sliderLabel.after(300,slider)
+
+def clock():
+    date=time.strftime("%d/%m/%Y")
+    currenttime=time.strftime("%H:%M:%S")
+    datetimeLabel.config(text=f'    Date:{date}\nTime:{currenttime}')
+    datetimeLabel.after(1000,clock)
+
+
+#GUI part
+
+root = ttkthemes.ThemedTk()
+
+root.get_themes()
+
+root.set_theme('radiance')
+
+root.geometry('1174x680+0+0')
+root.resizable(0, 0)
+root.title('Student Management System')
+
+datetimeLabel=Label(root,font=('times new roman',18,'bold'))
+datetimeLabel.place(x=5,y=5)
+clock()
+s='Student Management System'  #s[count]=t when count is 1
+sliderLabel=Label(root,text=s,font=('arial',28,'italic bold'),width=30)
+sliderLabel.place(x=200,y=0)
+slider()
+
+connectButton=ttk.Button(root,text='Connect database')
+connectButton.place(x=980,y=0)
+
+leftFrame=Frame(root)
+leftFrame.place(x=50,y=80,width=300,height=600)
+
+logo_image=PhotoImage(file='student.png')
+logo_label = Label(leftFrame,image=logo_image)
+logo_label.grid(row=0,column=0)
+
+addstudentButton=ttk.Button(leftFrame,text='Add Student',width=25,state=DISABLED)
+addstudentButton.grid(row=1,column=0,pady=20)
+
+searchstudentButton=ttk.Button(leftFrame,text='Search Student',width=25,state=DISABLED)
+searchstudentButton.grid(row=2,column=0,pady=20)
+
+deletestudentButton=ttk.Button(leftFrame,text='Delete Student',width=25,state=DISABLED)
+deletestudentButton.grid(row=3,column=0,pady=20)
+
+updatestudentButton=ttk.Button(leftFrame,text='Update Student',width=25,state=DISABLED)
+updatestudentButton.grid(row=4,column=0,pady=20)
+
+showstudentButton=ttk.Button(leftFrame,text='Show Student',width=25,state=DISABLED)
+showstudentButton.grid(row=5,column=0,pady=20)
+
+exportstudentButton=ttk.Button(leftFrame,text='Export Student',width=25,state=DISABLED)
+exportstudentButton.grid(row=6,column=0,pady=20)
+
+exitButton=ttk.Button(leftFrame,text='Exit',width=25,state=DISABLED)
+exitButton.grid(row=7,column=0,pady=20)
+
+rightFrame=Frame(root)
+rightFrame.place(x=350,y=80,width=820,height=600)
+
+scrollBarX=Scrollbar(rightFrame,orient=HORIZONTAL)
+scrollBarY=Scrollbar(rightFrame,orient=VERTICAL)
+
+studentTable=ttk.Treeview(rightFrame,columns=('Id','Name','Mobile No','Email','Address','Gender','DOB','Added Date'
+                                                  ,'Added Time'),
+                          xscrollcommand=scrollBarX.set,yscrollcommand=scrollBarY.set)
+
+scrollBarX.config(command=studentTable.xview)
+scrollBarY.config(command=studentTable.yview)
+
+scrollBarX.pack(side=BOTTOM,fill=X)
+scrollBarY.pack(side=RIGHT,fill=Y)
+
+studentTable.pack(fill=BOTH,expand=1)
+
+studentTable.heading('Id',text='Id')
+studentTable.heading('Name',text='Name')
+studentTable.heading('Mobile No',text='Mobile No')
+studentTable.heading('Email',text='Email Address')
+studentTable.heading('Address',text='Address')
+studentTable.heading('Gender',text='Gender')
+studentTable.heading('DOB',text='DOB')
+studentTable.heading('Added Date',text='Added Date')
+studentTable.heading('Added Time',text='Added Time')
+
+studentTable.config(show='headings')
+
+root.mainloop()
